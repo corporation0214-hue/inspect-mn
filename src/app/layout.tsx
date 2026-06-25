a9b214c1bc1b260bb9f24560e1ad44e1a80577f7
Munh So <munh.so@gmail.com>
2026-06-25 16:40:19 +0800

    Updated layout code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,28 +15,7 @@ export default async function RootLayout({
       className={theme === "dark" ? "dark" : ""}
       suppressHydrationWarning
     >
-      <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function () {
-                try {
-                  var theme = localStorage.getItem("theme");
-                  if (theme === "dark") {
-                    document.documentElement.classList.add("dark");
-                  } else {
-                    document.documentElement.classList.remove("dark");
-                  }
-                } catch (e) {}
-              })();
-            `,
-          }}
-        />
-      </head>
-
-      <body className="bg-slate-50 text-slate-900 dark:bg-slate-950 dark:text-slate-100">
-        {children}
-      </body>
+      <body className="bg-slate-50">{children}</body>
     </html>
   );
 }
